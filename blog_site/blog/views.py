26aba0a94d11a36d58a07db627ae365cd3112d4c
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-19 14:19:18 -0400

    update

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from .models import Post
+from .forms import CommentForm
+
 
 def frontpage(request):
     posts = Post.objects.all()
@@ -9,5 +11,16 @@ def frontpage(request):
 
 def post_detail(request, slug):
     post = Post.objects.get(slug=slug)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
 
-    return render(request, 'blog/post_detail.html', {'post': post})
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('post_detail', slug=post.slug)
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog/post_detail.html', {'post': post, 'form': form})
--- blog_site/blog_site/urls.py
+++ blog_site/blog_site/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog.views import frontpage
+from blog.views import frontpage, post_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', frontpage, name='frontpage')
+    path('', frontpage, name='frontpage'),
+    path('<slug:slug>/', post_detail, name='post_detail')
 ]
